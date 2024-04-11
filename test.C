25a76b495bc04989fc2ce6a0d28d7b294aadfb11
quelin-lechevranton <166115878+quelin-lechevranton@users.noreply.github.com>
2024-04-11 16:43:54 +0200

    test

--- test.C
+++ test.C
@@ -9,11 +9,12 @@ R__ADD_INCLUDE_PATH("nusimdata/v1_27_01/include/nusimdata/SimulationBase")
 
 int test(string file_name, int i_first_event, int i_last_event) {
 
+    int pdg = 13;
+
     art::InputTag depo_tag = InputTag("largeant:LArG4DetectorServicevolTPCActive:G4Stage1");
     art::InputTag monte_tag = InputTag("generator::SinglesGen");
     
-    gallery::Event ev("path/to/file.root");
-
+    gallery::Event ev(file_name);
 
     for ( ; !ev.atEnd() ; ev.next() ) {
         
@@ -22,28 +23,36 @@ int test(string file_name, int i_first_event, int i_last_event) {
         if ( i_event > i_last_event ) break;
 
         
-        
-        auto const truth_list = ev.getValidHandle<vector<simb::MCTruths>>(monte_tag);
-        auto const particle_list = ev.getValidHandle<vector<simb::MCParticles>>(monte_tag);
+        auto const depo_list = ev.getValidHandle<vector<sim::SimEnergyDeposits>>(depo_tag);
 
+        for (size_t i_depo=0; i_depo<depo_list->size(); i_depo++) {
 
-        for (size_t i_truth=0; i_truth<truth_list->size(); i_truth++) {
+            const sim::SimEnergyDeposit& depo = depo_list->at(i_depo);
+
+            if(depo.PdgCode() != pdg) {continue;}
+
+            auto Theta = depo.startPos.fCoordinates.Theta();
+
+            cout << "Theta :" << Theta << endl; 
+
 
         }
 
+        // auto const truth_list = ev.getValidHandle<vector<simb::MCTruths>>(monte_tag);
+        // auto const particle_list = ev.getValidHandle<vector<simb::MCParticles>>(monte_tag);
 
-        // auto const depo_list = ev.getValidHandle<vector<sim::SimEnergyDeposits>>(depo_tag);
-        // for (size_t i_depo=0; i_depo<depo_list->size(); i_depo++) {
+        // for (size_t i_truth=0; i_truth<truth_list->size(); i_truth++) {
+
+        //     const simb::MCTruths& truth = truth_list->at(i_truth);
+
+        //     if(truth.fPartList.fpdgCode != )
 
         // }
+
+
+
     }
-
-
-    Events->sim::SimEnergyDeposits_largeant_LArG4DetectorServicevolTPCActive_G4Stage1.obj.startPos.fCoordinates.Theta()
-
-
-
-Events->simb::MCTruths_cosmicgenerator__SinglesGen.obj.fPartList.Py()/simb::MCTruths_cosmicgenerator__SinglesGen.obj.fPartList.P()
-    
-
 }
+
+// Events->simb::MCTruths_cosmicgenerator__SinglesGen.obj.fPartList.Py()/simb::MCTruths_cosmicgenerator__SinglesGen.obj.fPartList.P()
+    
