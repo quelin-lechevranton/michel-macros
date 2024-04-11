d3db78223acc66737f5464efff49c9644905fcee
quelin-lechevranton <166115878+quelin-lechevranton@users.noreply.github.com>
2024-04-11 13:03:37 +0200

    Create test.C

---
+++ test.C
@@ -0,0 +1,15 @@
+R__ADD_INCLUDE_PATH("gallery/Event.h")
+R__ADD_INCLUDE_PATH("lardataobj/Simulation/SimEnergyDeposit.h")
+R__ADD_INCLUDE_PATH("larcoreobj/SimpleTypesAndConstants/geo_types.h")
+R__ADD_INCLUDE_PATH("larcoreobj/SimpleTypesAndConstants/geo_vectors.h")
+R__ADD_INCLUDE_PATH("nusimdata/v1_27_01/include/nusimdata/SimulationBase")
+#include "nusimdata/SimulationBase/MCTruth.h"
+#include "nusimdata/SimulationBase/MCParticle.h"
+#include "tools.h"
+
+int test(string file_name, int i_first_event, int i_last_event) {
+    art::InputTag depo_tag = InputTag("lar")
+    gallery::Event ev("path/to/file.root")
+    
+
+}
