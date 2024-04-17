e317dc3ae237f4d858c4ee7720f9e1dc8a61fb7a
Jérémy Quélin Lechevranton <jeremy.quelin-lechevranton@universite-paris-saclay.fr>
2024-04-17 16:51:26 +0200

    no main()

--- test.C
+++ test.C
@@ -14,16 +14,17 @@ art::InputTag point_tag("pandora");
 art::InputTag track_tag("pandoraTrack");
 
 
-int test(vector<string> file_list, int i_first_event, int i_last_event, int pdg);
+int plot(vector<string> file_list, int i_first_event, int i_last_event, int pdg);
 
-int main(int i_file=0, int i_first_event=0, int i_last_event=10, int pdg=13) {
+int test(int i_file=0, int i_first_event=0, int i_last_event=10, int pdg=13) {
     vector<string> files = ReadFileList(4,"file.list");
     vector<string> file_list = { files[i_file] };
     test(file_list,i_first_event,i_last_event,pdg);
     return 0;
 }
 
-int test(vector<string> file_list, int i_first_event, int i_last_event, int pdg) {
+
+int plot(vector<string> file_list, int i_first_event, int i_last_event, int pdg=13) {
 
 
     // TH3D* TH_depo = new TH3D("TH_depo",  //name
