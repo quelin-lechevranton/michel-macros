d8e5b7bf8a0320d073a3dbe2ee0dfb04fda9c76f
jeremy quelin-lechevranton <quelin-lechevranton@ssh-centos3.ijclab.in2p3.fr>
2024-04-25 10:19:22 +0200

    no R__ADD_INCLUDE_PATH

--- plot.C
+++ plot.C
@@ -1,8 +1,8 @@
-R__ADD_INCLUDE_PATH("gallery/Event.h")
-R__ADD_INCLUDE_PATH("lardataobj/Simulation/SimEnergyDeposit.h")
-R__ADD_INCLUDE_PATH("larcoreobj/SimpleTypesAndConstants/geo_types.h")
-R__ADD_INCLUDE_PATH("larcoreobj/SimpleTypesAndConstants/geo_vectors.h")
-R__ADD_INCLUDE_PATH("nusimdata/v1_27_01/include/nusimdata/SimulationBase")
+// R__ADD_INCLUDE_PATH("gallery/Event.h")
+// R__ADD_INCLUDE_PATH("lardataobj/Simulation/SimEnergyDeposit.h")
+// R__ADD_INCLUDE_PATH("larcoreobj/SimpleTypesAndConstants/geo_types.h")
+// R__ADD_INCLUDE_PATH("larcoreobj/SimpleTypesAndConstants/geo_vectors.h")
+// R__ADD_INCLUDE_PATH("nusimdata/v1_27_01/include/nusimdata/SimulationBase")
 #include "nusimdata/SimulationBase/MCTruth.h"
 #include "nusimdata/SimulationBase/MCParticle.h"
 #include "tools.h"
