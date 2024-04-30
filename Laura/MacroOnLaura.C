f275540850a5fbf86fd8038260e37acac6b983a2
jeremy quelin-lechevranton <quelin-lechevranton@ssh-centos3.ijclab.in2p3.fr>
2024-04-30 17:11:51 +0200

    empty

--- Laura/MacroOnLaura.C
+++ Laura/MacroOnLaura.C
@@ -6,120 +6,130 @@
 
 // const char filename[] = "/silver/DUNE/quelin-lechevranton/out/PDVD_10_muon_500MeV_LauraP_dumped.root";
 // const char filename[] = "/eos/user/t/thoudy/pdvd/sims/out/PDVD_10_muon_500MeV_LauraP_dumped.root";
-const char filename[] = "/eos/user/t/thoudy/pdvd/sims/out/pdvd_100_muon_1GeV_dumped.root";
+// const char filename[] = "/eos/user/t/thoudy/pdvd/sims/out/pdvd_100_muon_1GeV_dumped.root";
 // const char filename[] = "/eos/user/t/thoudy/pdvd/sims/out/PDVD_100_muon_800MeV_LauraP_dumped.root";
 
+vector<string> filelist = {
+    // "/silver/DUNE/quelin-lechevranton/out/PDVD_10_muon_500MeV_LauraP_dumped.root",
+    "/eos/user/t/thoudy/pdvd/sims/out/PDVD_10_muon_500MeV_LauraP_dumped.root",
+    "/eos/user/t/thoudy/pdvd/sims/out/pdvd_100_muon_1GeV_dumped.root",
+    "/eos/user/t/thoudy/pdvd/sims/out/PDVD_100_muon_800MeV_LauraP_dumped.root"
+};
+
 // int TrackEnds();
-// void Clusters();
-
-// void MacroOnLaura() {
-//     // TrackEnds();
-//     Clusters();
-
-// }
+void Clusters();
 
 void MacroOnLaura() {
-    TFile file(filename);
-    TTree *Reco = (TTree*) file.Get("LauraPDumper/Reco");
-    Int_t n_event=Reco->GetEntries();
+    // TrackEnds();
+    Clusters();
 
-    //POURQUOI PAS UN POINTER ?
-    unsigned int nParticles=0; 
+}
 
-    vector<int> *nClusters=nullptr,
-        *TrackID=nullptr,
-        *PdgCode=nullptr;
+void Clusters() {
+    
+    for (string filename : filelist) {
 
-    // vector<double> *TrackStartDirectionX=nullptr,
-    //     *TrackStartDirectionY=nullptr,
-    //     *TrackStartDirectionZ=nullptr,
-    //     *TrackVertexDirectionX=nullptr,
-    //     *TrackVertexDirectionY=nullptr,
-    //     *TrackVertexDirectionZ=nullptr;
+        TFile file(filename.c_str());
+        TTree *Reco = (TTree*) file.Get("LauraPDumper/Reco");
+        Int_t n_event=Reco->GetEntries();
 
-    vector<vector<double>*> TrackEnd = {nullptr,nullptr,nullptr};
+        //POURQUOI PAS UN POINTER ?
+        unsigned int nParticles=0; 
 
-    vector<vector<double>> *CluPlane=nullptr,
-        *CluView=nullptr,
-        *CluNHits=nullptr,
-        *CluSummedADC=nullptr,
-        *CluIntegral=nullptr,
-        *CluWidth=nullptr;
+        vector<int> *nClusters=nullptr,
+            *TrackID=nullptr,
+            *PdgCode=nullptr;
 
-    Reco->SetBranchAddress("pfpTrackEndX",   &(TrackEnd[0]));
-    Reco->SetBranchAddress("pfpTrackEndY",   &(TrackEnd[1]));
-    Reco->SetBranchAddress("pfpTrackEndZ",   &(TrackEnd[2]));
+        // vector<double> *TrackStartDirectionX=nullptr,
+        //     *TrackStartDirectionY=nullptr,
+        //     *TrackStartDirectionZ=nullptr,
+        //     *TrackVertexDirectionX=nullptr,
+        //     *TrackVertexDirectionY=nullptr,
+        //     *TrackVertexDirectionZ=nullptr;
 
-    Reco->SetBranchAddress("pfpNClusters",   &nClusters);
-    Reco->SetBranchAddress("nPFParticles",   &nParticles);
-    Reco->SetBranchAddress("pfpTrackID",     &TrackID);
-    Reco->SetBranchAddress("pfpPdgCode",     &PdgCode);
+        vector<vector<double>*> TrackEnd = {nullptr,nullptr,nullptr};
 
-    Reco->SetBranchAddress("pfpCluPlane",    &CluPlane);
-    Reco->SetBranchAddress("pfpCluView",     &CluView);
-    Reco->SetBranchAddress("pfpCluNHits",    &CluNHits);
-    Reco->SetBranchAddress("pfpCluSummedADC",&CluSummedADC);
-    Reco->SetBranchAddress("pfpCluIntegral", &CluIntegral);
-    Reco->SetBranchAddress("pfpCluWidth",    &CluWidth);
+        vector<vector<double>> *CluPlane=nullptr,
+            *CluView=nullptr,
+            *CluNHits=nullptr,
+            *CluSummedADC=nullptr,
+            *CluIntegral=nullptr,
+            *CluWidth=nullptr;
 
+        Reco->SetBranchAddress("pfpTrackEndX",   &(TrackEnd[0]));
+        Reco->SetBranchAddress("pfpTrackEndY",   &(TrackEnd[1]));
+        Reco->SetBranchAddress("pfpTrackEndZ",   &(TrackEnd[2]));
 
-    int n_bin=50, x_min=0, x_max=10000;
-    vector<TH1D*> histo(2);
-    histo[0] = new TH1D("hSum",";SummedADC/Width;count",n_bin,x_min,x_max);
-    histo[0]->SetLineColor(kRed+1);
-    histo[0]->SetLineWidth(2);
-    // histo[0]->SetName("muon CluSummedADC on collection");
-    // histo[0]->GetXaxis()->SetTitle("SummedADC");
-    // histo[0]->GetXaxis()->SetMaximum(1000);
-    // histo[0]->GetYaxis()->SetTitle("count");
+        Reco->SetBranchAddress("pfpNClusters",   &nClusters);
+        Reco->SetBranchAddress("nPFParticles",   &nParticles);
+        Reco->SetBranchAddress("pfpTrackID",     &TrackID);
+        Reco->SetBranchAddress("pfpPdgCode",     &PdgCode);
 
-    histo[1] = new TH1D("hInt",";Integral/Width;count",n_bin,x_min,x_max);
-    histo[1]->SetLineColor(kBlue-3);
-    histo[1]->SetLineWidth(2);
-    // histo[1]->SetName("muon CluIntegral on collection");
-    // histo[0]->GetXaxis()->SetTitle("Integral");
-    // histo[0]->GetXaxis()->SetMaximum(1000);
-
-    double Sum,Int,Width;
+        Reco->SetBranchAddress("pfpCluPlane",    &CluPlane);
+        Reco->SetBranchAddress("pfpCluView",     &CluView);
+        Reco->SetBranchAddress("pfpCluNHits",    &CluNHits);
+        Reco->SetBranchAddress("pfpCluSummedADC",&CluSummedADC);
+        Reco->SetBranchAddress("pfpCluIntegral", &CluIntegral);
+        Reco->SetBranchAddress("pfpCluWidth",    &CluWidth);
 
 
-    for (Int_t i_event=0; i_event < n_event; i_event++) {
-        // cout << "Event #" << i_event << ": ";
+        int n_bin=50, x_min=0, x_max=10000;
+        vector<TH1D*> histo(2);
+        histo[0] = new TH1D("hSum",";SummedADC/Width;count",n_bin,x_min,x_max);
+        histo[0]->SetLineColor(kRed+1);
+        histo[0]->SetLineWidth(2);
+        // histo[0]->SetName("muon CluSummedADC on collection");
+        // histo[0]->GetXaxis()->SetTitle("SummedADC");
+        // histo[0]->GetXaxis()->SetMaximum(1000);
+        // histo[0]->GetYaxis()->SetTitle("count");
 
-        Reco->GetEntry(i_event);
-        // cout << "\tn_track=" << TrackID->size();
-        // cout << "\tn_particle=" << nParticles;
-        // cout << "\tn_particle=" << nClusters->size();
+        histo[1] = new TH1D("hInt",";Integral/Width;count",n_bin,x_min,x_max);
+        histo[1]->SetLineColor(kBlue-3);
+        histo[1]->SetLineWidth(2);
+        // histo[1]->SetName("muon CluIntegral on collection");
+        // histo[0]->GetXaxis()->SetTitle("Integral");
+        // histo[0]->GetXaxis()->SetMaximum(1000);
 
-        for(Int_t i_part=0; i_part < nParticles; i_part++) {
-
-            if (PdgCode->at(i_part)!=13) continue;
-
-            for (Int_t i_clu=0; i_clu < CluPlane->at(i_part).size(); i_clu++) {
-
-                if(CluPlane->at(i_part)[i_clu]!=0) continue;
-
-                Sum=CluSummedADC->at(i_part)[i_clu];
-                Int=CluIntegral->at(i_part)[i_clu];
-                Width=CluWidth->at(i_part)[i_clu];
-
-                cout << "Event#" << i_event << "\tPart#" << i_part << "\tClu#" << i_clu << "\tSum=" << Sum << "\tInt=" << Int << "\tWidth=" << Width << endl;
+        double Sum,Int,Width;
 
 
-                histo[0]->Fill(Sum/Width);
-                histo[1]->Fill(Int/Width);
-                // cout << histo[0]->Fill(Sum) << " " << histo[1]->Fill(Int) << endl;
-            }
-        }
+        for (Int_t i_event=0; i_event < n_event; i_event++) {
+            // cout << "Event #" << i_event << ": ";
 
-        for(int i_track : *TrackID) {
+            Reco->GetEntry(i_event);
+            // cout << "\tn_track=" << TrackID->size();
+            // cout << "\tn_particle=" << nParticles;
+            // cout << "\tn_particle=" << nClusters->size();
+
+            for(Int_t i_part=0; i_part < nParticles; i_part++) {
+
+                if (PdgCode->at(i_part)!=13) continue;
+
+                for (Int_t i_clu=0; i_clu < CluPlane->at(i_part).size(); i_clu++) {
+
+                    if(CluPlane->at(i_part)[i_clu]!=0) continue;
+
+                    Sum=CluSummedADC->at(i_part)[i_clu];
+                    Int=CluIntegral->at(i_part)[i_clu];
+                    Width=CluWidth->at(i_part)[i_clu];
+
+                    cout << "Event#" << i_event << "\tPart#" << i_part << "\tClu#" << i_clu << "\tSum=" << Sum << "\tInt=" << Int << "\tWidth=" << Width << endl;
 
 
-        }
+                    histo[0]->Fill(Sum/Width);
+                    histo[1]->Fill(Int/Width);
+                    // cout << histo[0]->Fill(Sum) << " " << histo[1]->Fill(Int) << endl;
+                } //end of cluster loop
+            } //end of particule loop
 
-        // cout << endl;
-    } 
+            for(int i_track : *TrackID) {
 
+
+            } //end of track loop
+            // cout << endl;
+        } //end of event loop
+
+    } //end of file loop
 
     // Reco->GetEntry(9);
     // cout << "\tn_track=" << TrackID->size();
@@ -162,93 +172,94 @@ void MacroOnLaura() {
 
 
 
-int TrackEnds() {
-    TFile* file = TFile::Open(filename);
-    TTree* Reco=(TTree*) file->Get("LauraPDumper/Reco");
+// int TrackEnds() {
 
-    // if (i_event<0 || i_event>n_event) {
-    //     cout << "event index out of bound" << endl; 
-    //     file.Close();
-    //     return 1;
-    // }
+//     TFile* file = TFile::Open(filename);
+//     TTree* Reco=(TTree*) file->Get("LauraPDumper/Reco");
 
-    Int_t j_total=0;
+//     // if (i_event<0 || i_event>n_event) {
+//     //     cout << "event index out of bound" << endl; 
+//     //     file.Close();
+//     //     return 1;
+//     // }
 
-    // // vector<vector<double>*> TrackStart;
-    // // vector<vector<double>*> TrackEnd;
+//     Int_t j_total=0;
 
-    // // Reco->SetBranchAddress("pfpTrackStartX",&(TrackStart[0]));
-    // // Reco->SetBranchAddress("pfpTrackStartY",&(TrackStart[1]));
-    // // Reco->SetBranchAddress("pfpTrackStartZ",&(TrackStart[2]));
-    // // Reco->SetBranchAddress("pfpTrackEndX",&(TrackEnd[0]));
-    // // Reco->SetBranchAddress("pfpTrackEndY",&(TrackEnd[1]));
-    // // Reco->SetBranchAddress("pfpTrackEndZ",&(TrackEnd[2]));
+//     // // vector<vector<double>*> TrackStart;
+//     // // vector<vector<double>*> TrackEnd;
 
-    vector<double> *TrackStartX=0, *TrackStartY=0, *TrackStartZ=0, *TrackEndX=0, *TrackEndY=0, *TrackEndZ=0;
+//     // // Reco->SetBranchAddress("pfpTrackStartX",&(TrackStart[0]));
+//     // // Reco->SetBranchAddress("pfpTrackStartY",&(TrackStart[1]));
+//     // // Reco->SetBranchAddress("pfpTrackStartZ",&(TrackStart[2]));
+//     // // Reco->SetBranchAddress("pfpTrackEndX",&(TrackEnd[0]));
+//     // // Reco->SetBranchAddress("pfpTrackEndY",&(TrackEnd[1]));
+//     // // Reco->SetBranchAddress("pfpTrackEndZ",&(TrackEnd[2]));
+
+//     vector<double> *TrackStartX=0, *TrackStartY=0, *TrackStartZ=0, *TrackEndX=0, *TrackEndY=0, *TrackEndZ=0;
     
-    Reco->SetBranchAddress("pfpTrackStartX",&TrackStartX);
-    Reco->SetBranchAddress("pfpTrackStartY",&TrackStartY);
-    Reco->SetBranchAddress("pfpTrackStartZ",&TrackStartZ);
-    Reco->SetBranchAddress("pfpTrackEndX",&TrackEndX);
-    Reco->SetBranchAddress("pfpTrackEndY",&TrackEndY);
-    Reco->SetBranchAddress("pfpTrackEndZ",&TrackEndZ);
+//     Reco->SetBranchAddress("pfpTrackStartX",&TrackStartX);
+//     Reco->SetBranchAddress("pfpTrackStartY",&TrackStartY);
+//     Reco->SetBranchAddress("pfpTrackStartZ",&TrackStartZ);
+//     Reco->SetBranchAddress("pfpTrackEndX",&TrackEndX);
+//     Reco->SetBranchAddress("pfpTrackEndY",&TrackEndY);
+//     Reco->SetBranchAddress("pfpTrackEndZ",&TrackEndZ);
 
-    // vector<int> *TrackStartX=0;
-    // Reco->SetBranchAddress("pfpTrackEndZ",&TrackStartX);
-    // cout << (*TrackStartX).size() << endl;
+//     // vector<int> *TrackStartX=0;
+//     // Reco->SetBranchAddress("pfpTrackEndZ",&TrackStartX);
+//     // cout << (*TrackStartX).size() << endl;
 
-    // for (Int_t iev=0; iev < Reco->GetEntries(); ++iev) { //Loop over the events
-    //     Reco->GetEntry(iev);
-    //     cout<<"Treating event number: "<<iev<<endl;
-    // }
-    // file->Close();
+//     // for (Int_t iev=0; iev < Reco->GetEntries(); ++iev) { //Loop over the events
+//     //     Reco->GetEntry(iev);
+//     //     cout<<"Treating event number: "<<iev<<endl;
+//     // }
+//     // file->Close();
 
-    vector<TGraph2D*> graph(2);
-    graph[0] = new TGraph2D();
-    graph[0]->SetName("Track Start Positions");
-    graph[0]->SetMarkerColor(kBlue);
-    graph[0]->SetMarkerStyle(20);
-    graph[0]->SetMarkerSize(2);
+//     vector<TGraph2D*> graph(2);
+//     graph[0] = new TGraph2D();
+//     graph[0]->SetName("Track Start Positions");
+//     graph[0]->SetMarkerColor(kBlue);
+//     graph[0]->SetMarkerStyle(20);
+//     graph[0]->SetMarkerSize(2);
 
-    graph[1] = new TGraph2D();
-    graph[1]->SetName("Track End Positions");
-    graph[1]->SetMarkerColor(kRed);
-    graph[1]->SetMarkerStyle(20);
-    graph[1]->SetMarkerSize(2);
+//     graph[1] = new TGraph2D();
+//     graph[1]->SetName("Track End Positions");
+//     graph[1]->SetMarkerColor(kRed);
+//     graph[1]->SetMarkerStyle(20);
+//     graph[1]->SetMarkerSize(2);
 
-    graph[0]->GetXaxis()->SetTitle("X (cm)");
-    graph[0]->GetYaxis()->SetTitle("Y (cm)");
-    graph[0]->GetZaxis()->SetTitle("Z (cm)");
+//     graph[0]->GetXaxis()->SetTitle("X (cm)");
+//     graph[0]->GetYaxis()->SetTitle("Y (cm)");
+//     graph[0]->GetZaxis()->SetTitle("Z (cm)");
 
 
-    Int_t n_event = Reco->GetEntries();
-    for (Int_t i_event=0; i_event < n_event; i_event++) {
-        Reco->GetEntry(i_event);
+//     Int_t n_event = Reco->GetEntries();
+//     for (Int_t i_event=0; i_event < n_event; i_event++) {
+//         Reco->GetEntry(i_event);
 
-        for (size_t j=0; j< TrackStartX->size(); j++) {        
-            graph[0]->SetPoint(
-                j_total++,
-                TrackStartX->at(j),
-                TrackStartY->at(j),
-                TrackStartZ->at(j)
-            );
-            graph[1]->SetPoint(
-                j_total++,
-                TrackEndX->at(j),
-                TrackEndY->at(j),
-                TrackEndZ->at(j)
-            );
-        }
-    }
+//         for (size_t j=0; j< TrackStartX->size(); j++) {        
+//             graph[0]->SetPoint(
+//                 j_total++,
+//                 TrackStartX->at(j),
+//                 TrackStartY->at(j),
+//                 TrackStartZ->at(j)
+//             );
+//             graph[1]->SetPoint(
+//                 j_total++,
+//                 TrackEndX->at(j),
+//                 TrackEndY->at(j),
+//                 TrackEndZ->at(j)
+//             );
+//         }
+//     }
 
-    stringstream title;
-    // title << "Tracks Ends of Event #" << i_event;
-    title << "Tracks Ends";
-    TCanvas* canvas = new TCanvas("c",title.str().c_str());
-    canvas->cd();
-    graph[0]->Draw("AP");
-    graph[1]->Draw("P");     
+//     stringstream title;
+//     // title << "Tracks Ends of Event #" << i_event;
+//     title << "Tracks Ends";
+//     TCanvas* canvas = new TCanvas("c",title.str().c_str());
+//     canvas->cd();
+//     graph[0]->Draw("AP");
+//     graph[1]->Draw("P");     
 
-    file->Close();
-    return 0;
-}
+//     file->Close();
+//     return 0;
+// }
