fcc2f837962def89b652bc39e771d3398cad9945
jeremy quelin-lechevranton <quelin-lechevranton@ssh-centos3.ijclab.in2p3.fr>
2024-04-30 11:26:53 +0200

    empty

--- Laura/MacroOnLaura.C
+++ Laura/MacroOnLaura.C
@@ -62,15 +62,15 @@ int Clusters() {
 
 
     int n_bin=100, x_min=0, x_max=100000;
-    vector<TH1F*> histo(2);
-    histo[0] = new TH1F("hSum","CluSummedADC",n_bin,x_min,x_max);
+    vector<TH1D*> histo(2);
+    histo[0] = new TH1D("hSum","CluSummedADC",n_bin,x_min,x_max);
     histo[0]->SetLineColor(kRed+1);
     // histo[0]->SetName("muon CluSummedADC on collection");
     // histo[0]->GetXaxis()->SetTitle("SummedADC");
     // histo[0]->GetXaxis()->SetMaximum(1000);
     // histo[0]->GetYaxis()->SetTitle("count");
 
-    histo[1] = new TH1F("hInt","CluIntegral",n_bin,x_min,x_max);
+    histo[1] = new TH1D("hInt","CluIntegral",n_bin,x_min,x_max);
     histo[1]->SetLineColor(kBlue-3);
     // histo[1]->SetName("muon CluIntegral on collection");
     // histo[0]->GetXaxis()->SetTitle("Integral");
