e828df68726909fa3cd309e8f347c39dad520b84
Jérémy Quélin Lechevranton <quelin-lechevranton@ijclab.in2p3.fr>
2024-06-07 14:20:11 +0200

    empty

--- YAD/Bragg.C
+++ YAD/Bragg.C
@@ -9,9 +9,6 @@ const vector<string> filelist = yad::readFileList(n_file,"list/jeremy.list");
 void Bragg(size_t i=0) {
     clock_t start_time=clock();
 
-    TGraph* gdEdx = new TGraph();
-    // gdEdx->SetTitle("muon Energy Loss;Deposit Number;dEdx (Mev/cm)");    
-
     size_t i_file=0;
     for (string filename : filelist) {
         
@@ -21,7 +18,6 @@ void Bragg(size_t i=0) {
 
         size_t n_evt = T.GetEntries();
         for (size_t i_evt=0; i_evt < n_evt; i_evt++) {
-        // size_t i_evt=i; {
 
             cout << "Event#" << i_evt+1 << "/" << n_evt << "\r" << flush;
 
@@ -37,18 +33,14 @@ void Bragg(size_t i=0) {
                 if (!yad::isInside(T.DepX->at(i_prt),T.DepY->at(i_prt),T.DepZ->at(i_prt))) continue;
                 if (n_dep<n_bragg_integration+n_bragg_tail) continue;
 
-                // cout << i_evt << endl;
-
                 // for (size_t i_ppt=0; i_ppt < T.PrtNPt->at(i_prt); i_ppt++) {
 
                 // } //end particlepoint loop
 
                 double avg_dEdx=0;
                 for (size_t i_dep=n_dep-n_bragg_integration-n_bragg_tail; i_dep < n_dep-n_bragg_tail; i_dep++) {
-                    // gdEdx->AddPoint(n_dep-i_dep,(*T.DepE)[i_prt][i_dep]/0.03);
                     double E = (*T.DepE)[i_prt][i_dep];
                     avg_dEdx += E/0.03/n_bragg_integration;
-                    gdEdx->AddPoint(i_dep,E);
                 } //end deposit loop
                 cout << endl << "avg_dEdx=" << avg_dEdx << endl;
             } //end particle loop
@@ -58,7 +50,47 @@ void Bragg(size_t i=0) {
 
     // TCanvas* c1 = new TCanvas("c1","Bragg");
     // c1->cd();
-    // gdEdx->Draw();
 
     cout << "total time of execution: " << static_cast<double>(clock()-start_time)/CLOCKS_PER_SEC << " seconds" << endl;
+}
+
+void PlotBragg(size_t i_evt) {
+    clock_t start_time=clock();
+
+    TGraph* gdEdx = new TGraph();
+    // gdEdx->SetTitle("muon Energy Loss;Deposit Number;dEdx (Mev/cm)");    
+
+    size_t i_file=0;
+    for (string filename : filelist) {
+        
+        cout << "\e[3mOpening file #" << ++i_file << "/" << n_file << ": " << filename << "\e[0m" << endl;
+
+        yad::Truth T(filename.c_str());
+        T.GetEntry(i_evt);
+
+        for (size_t i_prt=0; i_prt < T.NPrt; i_prt++) {
+
+            int pdg = T.PrtPdg->at(i_prt);
+            size_t n_ppt = T.PrtNPt->at(i_prt);
+            size_t n_dep = T.PrtNDep->at(i_prt);
+
+            if (pdg!=13 && pdg!=-13) continue;
+            if (!yad::isInside(T.DepX->at(i_prt),T.DepY->at(i_prt),T.DepZ->at(i_prt))) continue;
+            if (n_dep<n_bragg_integration+n_bragg_tail) continue;
+
+            for (size_t i_dep=n_dep-n_bragg_integration-n_bragg_tail; i_dep < n_dep-n_bragg_tail; i_dep++) {
+                double E = (*T.DepE)[i_prt][i_dep];
+                gdEdx->AddPoint(i_dep,E);
+            } //end deposit loop
+        } //end particle loop
+    } //end file loop
+    cout << endl;
+
+    TCanvas* c1 = new TCanvas("c1","Bragg");
+    c1->cd();
+    gdEdx->Draw();
+
+    cout << "total time of execution: " << static_cast<double>(clock()-start_time)/CLOCKS_PER_SEC << " seconds" << endl;
+}
+
 }
