85e15bf86a6fd3e3e0b4984a3a8ff128e784c689
Jérémy Quélin Lechevranton <quelin-lechevranton@ijclab.in2p3.fr>
2024-06-07 13:14:26 +0200

    empty

--- YAD/PrtDep_Pt.C
+++ YAD/PrtDep_Pt.C
@@ -10,7 +10,9 @@ void PrtDep_Pt() {
     
     TH1D* hPrtDist = new TH1D("hPrtDist","Distance between two PrtPt;distance (cm);#",100,0,1);
     double avg_prt_distance=0;
-    TH1D* hNDepPt = new TH1D("hNDepPt","ratio NDep/NPt;ratio;#",100,0,100);
+    TH1D* hDepDist = new TH1D("hDepDist","Distance between two PrtDep;distance (cm);#",100,0,1);
+    double avg_dep_distance=0;
+    TH1D* hn_depPt = new TH1D("hn_depPt","ratio n_dep/NPt;ratio;#",100,0,100);
 
     size_t i_file=0;
     for (string filename : filelist) {
@@ -31,15 +33,15 @@ void PrtDep_Pt() {
                 int pdg = T.PrtPdg->at(i_prt);
                 if (pdg!=13 && pdg!=-13) continue;
 
-                size_t nppt = T.PrtNPt->at(i_prt);
-                size_t ndep = T.PrtNDep->at(i_prt);
+                size_t n_ppt = T.PrtNPt->at(i_prt);
+                size_t n_dep = T.Prtn_dep->at(i_prt);
 
-                hNDepPt->Fill((double) ndep/nppt);
+                hn_depPt->Fill((double) n_dep/n_ppt);
 
                 double X0 = (*T.PrtX)[i_prt][0];
                 double Y0 = (*T.PrtY)[i_prt][0];
                 double Z0 = (*T.PrtZ)[i_prt][0];
-                for (size_t i_ppt=1; i_ppt < nppt; i_ppt++) {
+                for (size_t i_ppt=1; i_ppt < n_ppt; i_ppt++) {
                     double X = (*T.PrtX)[i_prt][i_ppt];
                     double Y = (*T.PrtY)[i_prt][i_ppt];
                     double Z = (*T.PrtZ)[i_prt][i_ppt];
@@ -49,12 +51,24 @@ void PrtDep_Pt() {
 
                     X0=X; Y0=Y; Z0=Z;
                 } //end particlepoint loop
-                if (nppt>1) avg_prt_distance/=(nppt-1);
+                if (n_ppt>1) avg_prt_distance/=(n_ppt-1);
 
 
-                // for (size_t i_dep=0; i_dep < T.PrtNDep->at(i_prt); i_dep++) {
+                double X0 = (*T.DepX)[i_dep][0];
+                double Y0 = (*T.DepY)[i_dep][0];
+                double Z0 = (*T.DepZ)[i_dep][0];
+                for (size_t i_dep=0; i_dep < n_dep; i_dep++) {
+                    double X = (*T.DepX)[i_prt][i_dep];
+                    double Y = (*T.DepY)[i_prt][i_dep];
+                    double Z = (*T.DepZ)[i_prt][i_dep];
 
-                // } //end deposit loop
+                    hDepDist->Fill(yad::distance(X0,Y0,Z0,X,Y,Z));
+                    avg_dep_distance+=yad::distance(X0,Y0,Z0,X,Y,Z);
+
+                    X0=X; Y0=Y; Z0=Z;
+
+                } //end deposit loop
+                if (n_dep>1) avg_dep_distance/=(n_dep-1);
             } //end particle loop
 
         } //end event loop
@@ -67,7 +81,8 @@ void PrtDep_Pt() {
     c1->cd(1);
     hPrtDist->Draw("hist");
     c1->cd(2);
-    hNDepPt->Draw("hist");
+    hDepDist->Draw("hist");
+    // hn_depPt->Draw("hist");
 
     cout << "total time of execution: " << static_cast<double>(clock()-start_time)/CLOCKS_PER_SEC << " seconds" << endl;
 }
